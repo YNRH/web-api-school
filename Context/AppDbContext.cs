45326b075168d89031111e978ba3072057c02357
YNRH <yesica.rojas@tecsup.edu.pe>
2024-04-12 14:06:30 -0500

    Agregar archivos de proyecto.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_api_school.Models;
+
+namespace web_api_school.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Person> Persons { get; set; }
+
+    }
+
+
+}
---
+++ Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_api_school.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Apellido es obligatorio.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
+        public string FirstName { get; set; }
+
+        public int? Age { get; set; }
+    }
+}
